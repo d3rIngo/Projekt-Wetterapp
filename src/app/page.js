fae36b860c6c8426a40c751b4c4b996f87861025
d3rIngo <ingo.neubert92@gmx.de>
2024-03-20 12:42:09 +0100

    fix problem with screen at mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
+// Import der erforderlichen Module und Komponenten
 "use client"; // Markiert die Komponente als Client-Komponente
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { TiWeatherCloudy, TiWeatherSnow, TiWeatherWindy, TiWeatherShower, TiWeatherSunny } from 'react-icons/ti';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import React, { useState, useEffect } from 'react'; // Import von React-Hooks
+import axios from 'axios'; // Import für HTTP-Anfragen
+import { TiWeatherCloudy, TiWeatherSnow, TiWeatherWindy, TiWeatherShower, TiWeatherSunny } from 'react-icons/ti'; // Import von Wetter-Icons
+import { SpeedInsights } from "@vercel/speed-insights/next"; // Import für Geschwindigkeitsanalyse
 
 // Hauptkomponente für die Wetter-App
 export default function Home() {
   // Zustandsvariablen für Ort, Wetterdaten, Wetterklasse, Vorhersagedaten und Fetch-Status
-  const [location, setLocation] = useState("");
-  const [weatherData, setWeatherData] = useState(null);
-  const [weatherClass, setWeatherClass] = useState("");
-  const [forecastData, setForecastData] = useState(null); 
-  const [fetchingForecast, setFetchingForecast] = useState(false);
+  const [location, setLocation] = useState(""); // Zustand für den eingegebenen Ort
+  const [weatherData, setWeatherData] = useState(null); // Zustand für die aktuellen Wetterdaten
+  const [weatherClass, setWeatherClass] = useState(""); // Zustand für die Wetterklasse
+  const [forecastData, setForecastData] = useState(null); // Zustand für die Wettervorhersagedaten
+  const [fetchingForecast, setFetchingForecast] = useState(false); // Zustand für den Fetch-Status der Vorhersagedaten
 
   // Funktion zum Behandeln der Ortseingabe
   const handleLocationChange = (e) => {
@@ -37,12 +38,12 @@ export default function Home() {
         apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&lang=de`;
       }
 
-      const response = await axios.get(apiUrl);
+      const response = await axios.get(apiUrl); // HTTP-Anfrage an die Wetter-API
       const celsiusTemperature = kelvinToCelsius(response.data.main.temp);
       response.data.main.temp_c = celsiusTemperature;
-      setWeatherData(response.data);
+      setWeatherData(response.data); // Setzen der aktuellen Wetterdaten im Zustand
 
-      // Bestimme die Wetterklasse basierend auf der Wetterbeschreibung
+      // Bestimmung der Wetterklasse basierend auf der Wetterbeschreibung
       switch (response.data.weather[0].description.toLowerCase()) {
         case 'klarer himmel':
         case 'ein paar wolken':
@@ -72,7 +73,7 @@ export default function Home() {
           setWeatherClass("");
       }
 
-      // Direkter Aufruf der Vorhersage-Daten
+      // Direkter Aufruf der Vorhersagedaten
       await fetchForecastData();
       setFetchingForecast(true);
 
@@ -95,8 +96,8 @@ export default function Home() {
         apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&lang=de`;
       }
 
-      const response = await axios.get(apiUrl);
-      setForecastData(response.data.list);
+      const response = await axios.get(apiUrl); // HTTP-Anfrage an die Wetter-API für die Vorhersagedaten
+      setForecastData(response.data.list); // Setzen der Vorhersagedaten im Zustand
 
     } catch (error) {
       console.error(error);
@@ -106,13 +107,13 @@ export default function Home() {
   // Funktion zum Behandeln des Tastendrucks (Enter-Taste)
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      fetchWeatherData();
+      fetchWeatherData(); // Aufruf der Funktion zum Abrufen der Wetterdaten
     }
   };
 
   // Funktion zum Behandeln des Klicks auf den Button "Wetter abrufen"
   const handleButtonClick = () => {
-    fetchWeatherData();
+    fetchWeatherData(); // Aufruf der Funktion zum Abrufen der Wetterdaten
   };
 
   // Funktion zur Auswahl des Wettericons basierend auf der Wetterbeschreibung
@@ -129,92 +130,90 @@ export default function Home() {
       case 'leicht bedeckt':
       case 'überwiegend bedeckt':
       case 'überwiegend bewölkt':
-      case 'mäßig bewölkt':
-      case 'wolken':
         return <TiWeatherCloudy size={96} />;
-      case 'schnee':
-        return <TiWeatherSnow size={96} />;
-      case 'windig':
-        return <TiWeatherWindy size={96} />;
-      default:
-        return null;
-    }
-  };
-  
-  // Effekt-Hook zur Behandlung der Vorhersageaktualisierung
-  useEffect(() => {
-    if (fetchingForecast && location) {
-      fetchForecastData();
-    }
-  }, [fetchingForecast, location]);
-
-  // Hinzufügen des viewport-Meta-Tags für mobile Ansicht
-  useEffect(() => {
-    const metaTag = document.createElement('meta');
-    metaTag.name = 'viewport';
-    metaTag.content = 'width=device-width, initial-scale=1.0';
-    document.head.appendChild(metaTag);
-
-    return () => {
-      document.head.removeChild(metaTag);
+        case 'schnee':
+          return <TiWeatherSnow size={96} />;
+        case 'windig':
+          return <TiWeatherWindy size={96} />;
+        default:
+          return null;
+      }
     };
-  }, []);
-
-  // Rendern der Komponente mit JSX
-  return (
-    <div className={`container ${weatherClass}`} style={{ overflowY: 'auto', maxHeight: '100vh', minHeight: '100vh' }}> 
-      <h1>Wetter-App</h1>
-      <p>Gib einen Ort oder die PLZ ein und klicke danach auf &quot;Wetter abrufen&quot;, um das aktuelle Wetter zu sehen.</p>
-      <input
-        type="text"
-        value={location}
-        onChange={handleLocationChange}
-        onKeyPress={handleKeyPress}
-        placeholder="Ort oder Postleitzahl eingeben"
-      />
-      <button onClick={handleButtonClick}>Wetter abrufen</button>
-      {weatherData && (
-        <div>
-          <h2>Aktuelles Wetter für {weatherData.name}:</h2>
-          <p>Temperatur: {weatherData.main.temp_c.toFixed(1)}°C</p>
-          <p>Wetterzustand: {weatherData.weather[0].description}</p>
-          <p>Luftfeuchtigkeit: {weatherData.main.humidity}%</p>
-          <div className="weather-icon" style={{ textAlign: 'center' }}>
-            {getWeatherIcon(weatherData.weather[0].description)}
+    
+    // Effekt-Hook zur Behandlung der Vorhersageaktualisierung
+    useEffect(() => {
+      if (fetchingForecast && location) {
+        fetchForecastData(); // Aufruf der Funktion zum Abrufen der Wettervorhersagedaten
+      }
+    }, [fetchingForecast, location]);
+  
+    // Hinzufügen des viewport-Meta-Tags für mobile Ansicht
+    useEffect(() => {
+      const metaTag = document.createElement('meta'); // Erstellen eines Meta-Tags
+      metaTag.name = 'viewport';
+      metaTag.content = 'width=device-width, initial-scale=1.0'; // Festlegen der Ansichtsgröße
+      document.head.appendChild(metaTag); // Hinzufügen des Meta-Tags zum Dokumentenkopf
+  
+      return () => {
+        document.head.removeChild(metaTag); // Entfernen des Meta-Tags bei Aufruf der Aufräumfunktion
+      };
+    }, []);
+  
+    // Rendern der Komponente mit JSX
+    return (
+      <div className={`container ${weatherClass}`} style={{ overflowY: 'auto', maxHeight: '100vh' }}> 
+        <h1>Wetter-App</h1>
+        <p>Gib einen Ort oder die PLZ ein und klicke danach auf &quot;Wetter abrufen&quot;, um das aktuelle Wetter zu sehen.</p>
+        <input
+          type="text"
+          value={location}
+          onChange={handleLocationChange}
+          onKeyPress={handleKeyPress}
+          placeholder="Ort oder Postleitzahl eingeben"
+        />
+        <button onClick={handleButtonClick}>Wetter abrufen</button>
+        {weatherData && (
+          <div>
+            <h2>Aktuelles Wetter für {weatherData.name}:</h2>
+            <p>Temperatur: {weatherData.main.temp_c.toFixed(1)}°C</p>
+            <p>Wetterzustand: {weatherData.weather[0].description}</p>
+            <p>Luftfeuchtigkeit: {weatherData.main.humidity}%</p>
+            <div className="weather-icon" style={{ textAlign: 'center' }}>
+              {getWeatherIcon(weatherData.weather[0].description)}
+            </div>
           </div>
-        </div>
-      )}
-      {forecastData && (
-        <div>
-          <h2 style={{ marginTop: '40px' }}>3-Tage-Wettervorhersage:</h2>
-          <div style={{ height: '1px' }}></div> {/* Platz für den Abstand */}
-          {forecastData.slice(1, 4).map((forecast, index) => {
-            const minTemp = kelvinToCelsius(forecast.main.temp_min);
-            const maxTemp = kelvinToCelsius(forecast.main.temp_max);
-            const earlyMorningTemp = kelvinToCelsius(forecast.main.feels_like); // Temperatur am frühen Morgen
-            return (
-              <div key={index}>
-                <h3>
-                  {new Date(
-                    new Date().getTime() + (index + 1) * 24 * 60 * 60 * 1000
-                  ).toLocaleDateString("de-DE", { weekday: "long" })}
-                </h3>
-                <p>
-                  Temperatur:{" "}
-                  {earlyMorningTemp.toFixed(1)}°C -{" "}
-                  {maxTemp.toFixed(1)}°C
-                </p>
-                <p>Wetterzustand: {forecast.weather[0].description}</p>
-                <div className="weather-icon" style={{ textAlign: "center" }}>
-                  {getWeatherIcon(forecast.weather[0].description)}
+        )}
+        {forecastData && (
+          <div>
+            <h2 style={{ marginTop: '40px' }}>3-Tage-Wettervorhersage:</h2>
+            <div style={{ height: '1px' }}></div> {/* Platz für den Abstand */}
+            {forecastData.slice(1, 4).map((forecast, index) => {
+              const minTemp = kelvinToCelsius(forecast.main.temp_min);
+              const maxTemp = kelvinToCelsius(forecast.main.temp_max);
+              const earlyMorningTemp = kelvinToCelsius(forecast.main.feels_like); // Temperatur am frühen Morgen
+              return (
+                <div key={index}>
+                  <h3>
+                    {new Date(
+                      new Date().getTime() + (index + 1) * 24 * 60 * 60 * 1000
+                    ).toLocaleDateString("de-DE", { weekday: "long" })}
+                  </h3>
+                  <p>
+                    Temperatur:{" "}
+                    {earlyMorningTemp.toFixed(1)}°C -{" "}
+                    {maxTemp.toFixed(1)}°C
+                  </p>
+                  <p>Wetterzustand: {forecast.weather[0].description}</p>
+                  <div className="weather-icon" style={{ textAlign: "center" }}>
+                    {getWeatherIcon(forecast.weather[0].description)}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
-      <SpeedInsights url="https://wetter-now.vercel.app" />
-    </div>
-  );
-}
-
+              );
+            })}
+          </div>
+        )}
+        <SpeedInsights url="https://wetter-now.vercel.app" /> {/* Einbindung der Geschwindigkeitsanalyse */}
+      </div>
+    );
+  }
+  
